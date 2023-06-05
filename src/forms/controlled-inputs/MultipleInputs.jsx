4921ff3fecb7fc3804888173e5126a23ab738156
Code-with-Asif <asifdeveloper430@gmail.com>
2023-06-05 22:50:04 +0500

    made changes

--- src/forms/controlled-inputs/MultipleInputs.jsx
+++ src/forms/controlled-inputs/MultipleInputs.jsx
@@ -8,29 +8,65 @@ const MultipleInputs = () => {
     address: "",
     phone: "",
   });
+  const changeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(name, value);
+    setPerson({ ...person, [name]: value });
+  };
   return (
     <div className="mainParent">
       <h1>Controlled inputs</h1>
       <form>
         <div className="myUser">
           <label htmlFor="myName">Name:</label>
-          <input type="text" id="nyName" />
+          <input
+            type="text"
+            name="name"
+            id="nyName"
+            value={person.name}
+            onChange={changeHandler}
+          />
         </div>
         <div className="myUser">
           <label htmlFor="myAge">Age:</label>
-          <input type="text" id="myAge" />
+          <input
+            type="text"
+            name="age"
+            id="myAge"
+            value={person.age}
+            onChange={changeHandler}
+          />
         </div>
         <div className="myUser">
           <label htmlFor="myAddress">Address:</label>
-          <input type="text" id="myAddress" />
+          <input
+            type="text"
+            name="address"
+            id="myAddress"
+            value={person.address}
+            onChange={changeHandler}
+          />
         </div>
         <div className="myUser">
           <label htmlFor="myPhone">Phone:</label>
-          <input type="text" id="myPhone" />
+          <input
+            type="text"
+            name="phone"
+            id="myPhone"
+            value={person.phone}
+            onChange={changeHandler}
+          />
         </div>
         <div className="myUser">
           <label htmlFor="myEmail">Email:</label>
-          <input type="email" id="myEmail" />
+          <input
+            type="email"
+            name="email"
+            id="myEmail"
+            value={person.email}
+            onChange={changeHandler}
+          />
         </div>
         <button>Submit</button>
       </form>
